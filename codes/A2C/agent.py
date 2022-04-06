fe090fd361c0255bcf4bff097b241b4e4c9b4180
feigeZzzz <1822630344@qq.com>
2022-04-07 00:10:37 +0800

    [reinforce][优化代码结构]

--- codes/A2C/agent.py
+++ codes/A2C/agent.py
@@ -14,9 +14,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Categorical
 
+
 class ActorCritic(nn.Module):
     ''' A2C网络模型，包含一个Actor和Critic
     '''
+
     def __init__(self, input_dim, output_dim, hidden_dim):
         super(ActorCritic, self).__init__()
         self.critic = nn.Sequential(
@@ -24,29 +26,32 @@ class ActorCritic(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_dim, 1)
         )
-        
+
         self.actor = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, output_dim),
             nn.Softmax(dim=1),
         )
-        
+
     def forward(self, x):
         value = self.critic(x)
         probs = self.actor(x)
-        dist  = Categorical(probs)
+        dist = Categorical(probs)
         return dist, value
+
+
 class A2C:
     ''' A2C算法
     '''
-    def __init__(self,state_dim,action_dim,cfg) -> None:
+
+    def __init__(self, state_dim, action_dim, cfg) -> None:
         self.gamma = cfg.gamma
         self.device = cfg.device
         self.model = ActorCritic(state_dim, action_dim, cfg.hidden_size).to(self.device)
         self.optimizer = optim.Adam(self.model.parameters())
 
-    def compute_returns(self,next_value, rewards, masks):
+    def compute_returns(self, next_value, rewards, masks):
         R = next_value
         returns = []
         for step in reversed(range(len(rewards))):
