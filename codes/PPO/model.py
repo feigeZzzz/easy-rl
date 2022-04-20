64f35c387d5fcff70a1d0e9daa2ec2111d39f2af
feigeZzzz <1822630344@qq.com>
2022-04-20 18:47:14 +0800

    [reinforce][增加PPO新脚本]

--- codes/PPO/model.py
+++ codes/PPO/model.py
@@ -10,6 +10,8 @@ Environment:
 
 import torch.nn as nn
 from torch.distributions.categorical import Categorical
+import torch
+import torch.nn.functional as F
 
 
 class Actor(nn.Module):
@@ -30,6 +32,44 @@ class Actor(nn.Module):
         dist = self.actor(state)
         dist = Categorical(dist)
         return dist
+
+
+class ActorNet(nn.Module):
+    def __init__(self, n_states, bound):
+        super(ActorNet, self).__init__()
+        self.n_states = n_states
+        self.bound = bound
+
+        self.layer = nn.Sequential(
+            nn.Linear(self.n_states, 128),
+            nn.ReLU()
+        )
+
+        self.mu_out = nn.Linear(128, 1)
+        self.sigma_out = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = F.relu(self.layer(x))
+        mu = self.bound * torch.tanh(self.mu_out(x))
+        sigma = F.softplus(self.sigma_out(x))
+        dist = torch.distributions.Normal(mu, sigma)
+        return dist
+
+
+class CriticNet(nn.Module):
+    def __init__(self, n_states):
+        super(CriticNet, self).__init__()
+        self.n_states = n_states
+
+        self.layer = nn.Sequential(
+            nn.Linear(self.n_states, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+    def forward(self, x):
+        v = self.layer(x)
+        return v
 
 
 class Critic(nn.Module):
